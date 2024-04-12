c7f4981ac3a300c83c5b4ea78be765956d5f15fe
jesuswav <garciajesse2003@gmail.com>
2024-04-12 10:02:16 -0600

    responsive desing for card grid on cabins page

---
+++ src/services/data.js
@@ -0,0 +1,32 @@
+const cabinList = [
+  {
+    id: 0,
+    name: 'Cabaña Uno',
+    rooms: 4,
+    price: 2400,
+    url: '../../public/images/cabana-de-madera.png',
+  },
+  {
+    id: 1,
+    name: 'Cabaña Dos',
+    rooms: 4,
+    price: 2400,
+    url: '../../public/images/cabana-de-playa.png',
+  },
+  {
+    id: 2,
+    name: 'Cabaña Tres',
+    rooms: 4,
+    price: 2400,
+    url: '../../public/images/cabana.png',
+  },
+  {
+    id: 3,
+    name: 'Cabaña Cuatro',
+    rooms: 4,
+    price: 2400,
+    url: '../../public/images/cabina.png',
+  },
+]
+
+export default cabinList
