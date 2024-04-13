e3bbaa391d8662b34054efd803d8168faa796eb6
jesuswav <garciajesse2003@gmail.com>
2024-04-13 12:17:29 -0600

    description for all the cabins

--- src/services/data.js
+++ src/services/data.js
@@ -2,7 +2,8 @@ const cabinList = [
   {
     id: 0,
     name: 'Cabaña Uno',
-    description: 'Description',
+    description:
+      'Una acogedora cabaña ideal para escapadas familiares o reuniones con amigos. Con dos amplias habitaciones, ofrece espacio más que suficiente para grupos de hasta 8 personas. Su diseño cálido y rústico crea el ambiente perfecto para relajarse y disfrutar de la naturaleza. ¡Trae a tu mascota y comparte momentos inolvidables juntos!',
     peopleCapacity: 8,
     petFriendly: true,
     rooms: 2,
@@ -12,7 +13,8 @@ const cabinList = [
   {
     id: 1,
     name: 'Cabaña Dos',
-    description: 'Description',
+    description:
+      'Sumérgete en el encanto costero con esta espaciosa cabaña de playa. Con cuatro cómodas habitaciones, es ideal para grandes grupos que buscan un refugio junto al mar. Su diseño luminoso y aireado te invita a relajarte y disfrutar de las vistas panorámicas. Aprovecha al máximo tu estancia con la comodidad de traer a tu mascota contigo.',
     peopleCapacity: 8,
     petFriendly: true,
     rooms: 4,
@@ -22,7 +24,8 @@ const cabinList = [
   {
     id: 2,
     name: 'Cabaña Tres',
-    description: 'Description',
+    description:
+      'Experimenta la serenidad de la naturaleza en esta encantadora cabaña. Con tres acogedoras habitaciones, ofrece un ambiente tranquilo y confortable para tu escapada. Su diseño elegante y funcional te brinda todo lo que necesitas para relajarte y reconectarte con tus seres queridos. ¡No dejes atrás a tu mascota y disfruta de momentos inolvidables juntos!',
     peopleCapacity: 8,
     petFriendly: true,
     rooms: 3,
@@ -32,7 +35,8 @@ const cabinList = [
   {
     id: 3,
     name: 'Cabaña Cuatro',
-    description: 'Description',
+    description:
+      'Escapa del ajetreo de la vida cotidiana y sumérgete en la tranquilidad de esta encantadora cabaña. Con una habitación íntima y acogedora, es perfecta para parejas o pequeños grupos en busca de privacidad y comodidad. Su diseño minimalista y funcional te permite desconectar y disfrutar de la naturaleza. Además, ¡puedes traer a tu mascota para que disfrute contigo de esta experiencia única!',
     peopleCapacity: 8,
     petFriendly: true,
     rooms: 1,
