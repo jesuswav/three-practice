68652022ef0d75e9edc2d1cd72e478f0455ebce2
jesuswav <garciajesse2003@gmail.com>
2024-04-16 23:45:04 -0600

    first version of cabin detailed page

--- src/services/data.js
+++ src/services/data.js
@@ -8,7 +8,13 @@ const cabinList = [
     petFriendly: true,
     rooms: 2,
     price: 2400,
-    url: '../../public/images/cabana-de-madera.png',
+    images: [
+      'https://images.pexels.com/photos/5644485/pexels-photo-5644485.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/8241337/pexels-photo-8241337.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/8961452/pexels-photo-8961452.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/18649992/pexels-photo-18649992/free-photo-of-campo-montana-granja-animales.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/13856367/pexels-photo-13856367.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
   },
   {
     id: 1,
@@ -19,7 +25,13 @@ const cabinList = [
     petFriendly: true,
     rooms: 4,
     price: 2400,
-    url: '../../public/images/cabana-de-playa.png',
+    images: [
+      'https://images.pexels.com/photos/6731722/pexels-photo-6731722.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/3714260/pexels-photo-3714260.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/4245291/pexels-photo-4245291.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/14148262/pexels-photo-14148262.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/12311980/pexels-photo-12311980.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
   },
   {
     id: 2,
@@ -30,7 +42,13 @@ const cabinList = [
     petFriendly: true,
     rooms: 3,
     price: 2400,
-    url: '../../public/images/cabana.png',
+    images: [
+      'https://images.pexels.com/photos/15656391/pexels-photo-15656391/free-photo-of-paisaje-montanas-naturaleza-casas.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/2290439/pexels-photo-2290439.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/5300210/pexels-photo-5300210.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/18026360/pexels-photo-18026360/free-photo-of-mar-casas-verano-costa.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/12699620/pexels-photo-12699620.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
   },
   {
     id: 3,
@@ -41,7 +59,13 @@ const cabinList = [
     petFriendly: true,
     rooms: 1,
     price: 2400,
-    url: '../../public/images/cabina.png',
+    images: [
+      'https://images.pexels.com/photos/800349/pexels-photo-800349.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/19780235/pexels-photo-19780235/free-photo-of-nieve-nevar-paisaje-montanas.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/19736319/pexels-photo-19736319/free-photo-of-resfriado-frio-nieve-nevar.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/8037875/pexels-photo-8037875.jpeg?auto=compress&cs=tinysrgb&w=600',
+      'https://images.pexels.com/photos/10998394/pexels-photo-10998394.jpeg?auto=compress&cs=tinysrgb&w=600',
+    ],
   },
 ]
 
